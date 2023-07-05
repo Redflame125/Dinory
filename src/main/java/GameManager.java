7e13ad385956e6070d58a7383e69e90d717919eb
Seraphin Berger <MCmoderSD@web.de>
2023-07-05 20:07:35 +0200

    Initialisation Done

---
+++ src/main/java/GameManager.java
@@ -0,0 +1,8 @@
+public class GameManager {
+    private final GameUI ui;
+    public GameManager(GameUI ui) {
+        this.ui = ui;
+    }
+
+    // ToDo Mach Game Logic und Ansagen!!!
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Set waiting time in milliseconds:");
-        //Scanner scanner = new Scanner(System.in);
-        //new GameUI(scanner.nextInt());
-        new GameUI(1000);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bitte geben Sie die Wartezeit ein, wie lange die Karten aufgedeckt bleiben: ");
+        int waitPeriod = scanner.nextInt() * 1000;
+
+        System.out.println("Bitte geben Sie die Anzahl der Spieler ein: ");
+        int playerCount = scanner.nextInt();
+
+        AtomicReferenceArray<String> playerNames = new AtomicReferenceArray<>(new String[playerCount]);
+
+
+        for (int i = 0; i < playerCount; i++) {
+            System.out.println("Bitte geben Sie den Namen des " + (i + 1) + ". Spielers ein: ");
+            playerNames.set(i, scanner.next());
+        }
+
+        new GameUI(waitPeriod, playerCount, playerNames);
     }
 }
